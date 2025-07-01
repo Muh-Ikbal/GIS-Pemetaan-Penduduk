dcaabc24754050a08a87303bbe0c553a8da980f4
Nouzen-s <ryuu.seryu@gmail.com>
2025-07-01 13:24:50 +0800

    change vaite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','resources/js/index.jsx'],
             refresh: true,
         }),
         react(),
