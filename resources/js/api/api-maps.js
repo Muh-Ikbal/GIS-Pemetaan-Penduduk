968f6d5a32eac55f197e924f72b458c627bb5381
Nouzen-s <ryuu.seryu@gmail.com>
2025-07-10 01:24:19 +0800

    deploy railway2

--- resources/js/api/api-maps.js
+++ resources/js/api/api-maps.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = "https://gis-pemetaan-penduduk-production-fa51.up.railway.app/";
+export const API_URL = "https://gis-pemetaan-penduduk-production-fa51.up.railway.app/api/";
 
 
 
