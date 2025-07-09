97fdb265e3dbbf9cf388b243158ec3185a1afd1f
Nouzen-s <ryuu.seryu@gmail.com>
2025-07-10 01:42:12 +0800

    api

--- resources/js/api/api-maps.js
+++ resources/js/api/api-maps.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // export const API_URL = import.meta.env.VITE_API_URL;
-export const API_URL = "https://gis-pemetaan-penduduk-production-fa51.up.railway.app/api";
+export const API_URL = "api";
 
 console.log(API_URL)
 export async function getMaps(tahun = '') {
