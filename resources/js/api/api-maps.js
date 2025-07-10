c34b9d3f25f7387cd924025f3aaea21b7c45df09
Nouzen-s <ryuu.seryu@gmail.com>
2025-07-10 08:23:06 +0800

    api2

--- resources/js/api/api-maps.js
+++ resources/js/api/api-maps.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
-const API_URL = `https://gis-pemetaan-penduduk-production-fa51.up.railway.app/data`;
+const api = `https://gis-pemetaan-penduduk-production-fa51.up.railway.app/data`;
 
 export async function getMaps(tahun = '') {
-    console.log("app url: ",API_URL)
+    console.log("app url: ",api)
 
-    return axios.get(`${API_URL}/data-penduduk`)
+    return axios.get(`${api}/data-penduduk`)
         .then(response => response.data)
         .catch(error => {
         console.error('Error fetching maps:', error);
@@ -13,7 +13,7 @@ export async function getMaps(tahun = '') {
 }
 
 export async function getKecamatan(){
-    return axios.get(`${API_URL}/data-kecamatan`).then(response => response.data)
+    return axios.get(`${api}/data-kecamatan`).then(response => response.data)
     .catch(error => {
         console.error('Error fetching kecamatan:', error);
         throw error;
@@ -22,7 +22,7 @@ export async function getKecamatan(){
 
 export async function getDetailKecamatan(id){
     try{
-        const response = await axios.get(`${API_URL}/kecamatan/` + id);
+        const response = await axios.get(`${api}/kecamatan/` + id);
         return response.data;
     }catch(err){
         console.error('Error fetching detail kecamatan:', err);
